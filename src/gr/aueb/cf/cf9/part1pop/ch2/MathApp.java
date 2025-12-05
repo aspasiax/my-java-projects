fad11239bf0a0ecd735d09cfcad1b1c2e7494010
aspasiax <asxafi@gmail.com>
2025-12-05 11:36:33 +0200

    feat: add MathApp demo for min and max methods

---
+++ src/gr/aueb/cf/cf9/part1pop/ch2/MathApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.cf9.part1pop.ch2;
+
+/**
+ * Demonstrates basic usage of the {@link Math} class.
+ * Specifically, it shows how to find the minimum and maximum
+ * of two numbers using Math.min() and Math.max().
+ */
+
+public class MathApp {
+
+    public static void main(String[] args) {
+        System.out.println("The smaller between 3 and 5 is: " + Math.min(3, 5));
+        System.out.println("The larger between 3 and 5 is: " + Math.max(3, 5));
+    }
+}
