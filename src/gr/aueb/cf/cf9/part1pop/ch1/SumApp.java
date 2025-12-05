52053b4b1a1a329b89d4d653ef5547d9f856fe07
aspasiax <asxafi@gmail.com>
2025-12-05 10:44:15 +0200

    feat: add SumApp demo for integer sum

---
+++ src/gr/aueb/cf/cf9/part1pop/ch1/SumApp.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.cf9.part1pop.ch1;
+
+/**
+ * Calculates the sum of two integers (19 and 30)
+ * and prints the result to the console.
+ * The expected result is 49.
+ */
+
+public class SumApp {
+
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int num1 = 19;
+        int num2 = 30;
+        int sum = 0;
+
+        // Operation: Calculate the sum
+        sum = num1 + num2;
+
+        // Output the result to the console
+        System.out.println("The result of the addition is equal to " + sum);
+    }
+}
