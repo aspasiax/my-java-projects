44b5b8618031ceb7cda30ede4ecb014d4e4659fa
aspasiax <asxafi@gmail.com>
2025-12-06 10:15:03 +0200

    feat: add Boolean Demo

---
+++ src/gr/aueb/cf/cf9/part1pop/ch3/BoolApp.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.cf9.part1pop.ch3;
+
+/**
+ * Boolean Demo
+ */
+
+public class BoolApp {
+
+    public static void main(String[] args) {
+        boolean isRaining = true;
+        boolean isSunny = false;
+        boolean isHot = true;
+        boolean isCold = false;
+    }
+}
+
